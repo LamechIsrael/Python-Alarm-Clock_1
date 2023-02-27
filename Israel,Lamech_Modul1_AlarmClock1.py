1984b05bef0bb8a1f6bba05d0a9ea28ced0284d0
Lamech A Israel <103595802+LamechIsrael@users.noreply.github.com>
2023-02-27 12:54:25 -0600

    Finished my first python project

---
+++ Israel,Lamech_Modul1_AlarmClock1.py
@@ -0,0 +1,44 @@
+
+
+#########################################
+# CS 1110A - Programming in Python      #
+# Module 1 - Exercise 1 - Alarm Clock 1 #
+# Author: Lamech Israel                 #
+# Date:   01/10/2022                    #
+#########################################
+
+print('Alarm Clock, Part 1')
+
+# Input
+
+currentTime = 5
+amPm = 'am'
+
+print('\nRight now, it is', currentTime,amPm)
+
+# Processing
+
+alarmHours = 83
+alarmDays = int(alarmHours/24)
+alarmRemander = alarmHours%24
+
+alarmTime = currentTime + alarmRemander
+
+
+if alarmTime > 11:
+    if amPm == 'am':
+        amPm = 'pm'
+    else :
+        amPm = 'am'
+
+if alarmTime >= 12:
+    alarmTime = alarmTime % 12
+    
+#Output
+    
+
+print('The alarm clock will ring after',alarmHours,'hours which is',alarmDays,'days from now at',alarmTime,amPm)
+
+print('\nDone!')
+    
+
